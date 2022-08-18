8ed43059db6de1d5a06552e8d7260a222c1c8e44
jsp-hashicorp <jsp@hashicorp.com>
2022-08-18 14:05:03 +0900

    change nodejs10.x to nodejs16.x

--- lambda.tf
+++ lambda.tf
@@ -26,7 +26,7 @@ resource "aws_lambda_function" "example" {
    # is the name of the property under which the handler function was
    # exported in that file.
    handler = "main.handler"
-   runtime = "nodejs10.x"
+   runtime = "nodejs16.x"
 
    role = aws_iam_role.lambda_exec.arn
 }
